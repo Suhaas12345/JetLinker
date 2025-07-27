63c50ad5bcf7c32c20e9d5aed22a1c6eae80a1ee
Suhas <142965982+Suhaas12345@users.noreply.github.com>
2025-07-27 15:21:58 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,218 +1,295 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <climits>
-#include <cstdlib>
-#include <ctime>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-// Structure to represent a city
-struct City {
+class bank{
+    public:
     string name;
-    vector<pair<int, int>> connections; // pair represents (city_index, duration)
-};
+    int netAmount;
+    set<string> types;
+}; 
 
-// Function to find the shortest path between two cities using Dijkstra's algorithm
-vector<int> dijkstra(vector<City>& cities, int start, int end, vector<int>& predecessors) {
-    int numCities = cities.size();
-    vector<int> distance(numCities, INT_MAX); // Initialize distances to infinity
-    vector<bool> visited(numCities, false); // Initialize visited array
-
-    distance[start] = 0; // Distance from start city to itself is 0
-
-    for (int i = 0; i < numCities - 1; i++) {
-        int minDistance = INT_MAX;
-        int minIndex;
-
-        // Find the city with the minimum distance
-        for (int j = 0; j < numCities; j++) {
-            if (!visited[j] && distance[j] <= minDistance) {
-                minDistance = distance[j];
-                minIndex = j;
-            }
-        }
-
-        visited[minIndex] = true; // Mark the city as visited
-
-        // Update the distances and predecessors of the neighboring cities
-        for (const auto& connection : cities[minIndex].connections) {
-            int neighbor = connection.first;
-            int duration = connection.second;
-            if (!visited[neighbor] && distance[minIndex] != INT_MAX && distance[minIndex] + duration < distance[neighbor]) {
-                distance[neighbor] = distance[minIndex] + duration;
-                predecessors[neighbor] = minIndex;
-            }
+int getMinIndex(bank listOfNetAmounts[],int numBanks){
+    int min=INT_MAX, minIndex=-1;
+    for(int i=0;i<numBanks;i++){
+        if(listOfNetAmounts[i].netAmount == 0) continue;
+        
+        if(listOfNetAmounts[i].netAmount < min){
+            minIndex = i;
+            min = listOfNetAmounts[i].netAmount;
         }
     }
-
-    return distance;
+    return minIndex;
 }
 
-// Function to find the shortest paths between all cities using Floyd-Warshall algorithm
-vector<vector<int>> floydWarshall(vector<City>& cities) {
-    int numCities = cities.size();
-    vector<vector<int>> dist(numCities, vector<int>(numCities, INT_MAX));
-
-    // Initialize distance matrix with direct connections
-    for (int i = 0; i < numCities; i++) {
-        dist[i][i] = 0;
-        for (const auto& connection : cities[i].connections) {
-            int neighbor = connection.first;
-            int duration = connection.second;
-            dist[i][neighbor] = duration;
+int getSimpleMaxIndex(bank listOfNetAmounts[],int numBanks){
+    int max=INT_MIN, maxIndex=-1;
+    for(int i=0;i<numBanks;i++){
+        if(listOfNetAmounts[i].netAmount == 0) continue;
+        
+        if(listOfNetAmounts[i].netAmount > max){
+            maxIndex = i;
+            max = listOfNetAmounts[i].netAmount;
         }
     }
+    return maxIndex;
+}
 
-    // Floyd-Warshall algorithm
-    for (int k = 0; k < numCities; k++) {
-        for (int i = 0; i < numCities; i++) {
-            for (int j = 0; j < numCities; j++) {
-                if (dist[i][k] != INT_MAX && dist[k][j] != INT_MAX && dist[i][k] + dist[k][j] < dist[i][j]) {
-                    dist[i][j] = dist[i][k] + dist[k][j];
+pair<int,string> getMaxIndex(bank listOfNetAmounts[],int numBanks,int minIndex,bank input[],int maxNumTypes){
+    int max=INT_MIN;
+    int maxIndex=-1;
+    string matchingType;
+    
+    for(int i=0;i<numBanks;i++){
+        if(listOfNetAmounts[i].netAmount == 0) continue;
+        
+        if(listOfNetAmounts[i].netAmount < 0) continue;
+        
+        //TODO 
+        //see complexity of intersection
+        
+        vector<string> v(maxNumTypes);
+        vector<string>::iterator ls=set_intersection(listOfNetAmounts[minIndex].types.begin(),listOfNetAmounts[minIndex].types.end(), listOfNetAmounts[i].types.begin(),listOfNetAmounts[i].types.end(), v.begin());
+        
+        if((ls-v.begin())!=0 && max<listOfNetAmounts[i].netAmount ){
+            max=listOfNetAmounts[i].netAmount;
+            maxIndex=i;
+            matchingType = *(v.begin());
+        } 
+    }
+    
+    //if there is NO such max which has a common type with any remaining banks then maxIndex has -1
+    // also return the common payment type
+    return make_pair(maxIndex,matchingType);
+}
+
+void printAns(vector<vector<pair<int,string>>> ansGraph, int numBanks,bank input[]){
+    
+    cout<<"\nThe transactions for minimum cash flow are as follows : \n\n";
+    for(int i=0;i<numBanks;i++){
+        for(int j=0;j<numBanks;j++){
+            
+            if(i==j) continue;
+            
+            if(ansGraph[i][j].first != 0 && ansGraph[j][i].first != 0){
+                
+                if(ansGraph[i][j].first == ansGraph[j][i].first){
+                    ansGraph[i][j].first=0;
+                    ansGraph[j][i].first=0;
+                }
+                else if(ansGraph[i][j].first > ansGraph[j][i].first){
+                    ansGraph[i][j].first -= ansGraph[j][i].first; 
+                    ansGraph[j][i].first =0;
+                    
+                    cout<<input[i].name<<" pays Rs" << ansGraph[i][j].first<< "to "<<input[j].name<<" via "<<ansGraph[i][j].second<<endl;
+                }
+                else{
+                    ansGraph[j][i].first -= ansGraph[i][j].first;
+                    ansGraph[i][j].first = 0;
+                    
+                    cout<<input[j].name<<" pays Rs "<< ansGraph[j][i].first<<" to "<<input[i].name<<" via "<<ansGraph[j][i].second<<endl;
+                    
                 }
             }
-        }
-    }
-
-    return dist;
-}
-
-// Function to check weather and return safety status
-string checkWeather(int temperature) {
-    if (temperature > 25 || temperature < 5) {
-        return "UNSAFE";
-    } else {
-        return "SAFE";
-    }
-}
-
-// Function to check if the fuel is sufficient for the journey
-string checkFuel(int fuel) {
-    if (fuel >= 1000) {
-        return "Good to Go!";
-    } else {
-        return "INSUFFICIENT FUEL";
-    }
-}
-
-int main() {
-    srand(time(0)); // Seed the random number generator
-
-    int numCities;
-    cout << "Enter the number of cities: ";
-    cin >> numCities;
-
-    vector<City> cities(numCities);
-
-    // Initialize city names
-    for (int i = 0; i < numCities; i++) {
-        cout << "Enter the name of city " << i + 1 << ": ";
-        cin >> cities[i].name;
-    }
-
-    // Initialize city connections and durations with user input
-    for (int i = 0; i < numCities; i++) {
-        for (int j = i + 1; j < numCities; j++) {
-            int duration;
-            cout << "Enter the duration (in minutes) from " << cities[i].name << " to " << cities[j].name << ": ";
-            cin >> duration;
-            cities[i].connections.push_back(make_pair(j, duration));
-            cities[j].connections.push_back(make_pair(i, duration));
-        }
-    }
-
-    // Print city connections and durations
-    cout << "City Connections:" << endl;
-    for (int i = 0; i < numCities; i++) {
-        cout << "Connections from " << cities[i].name << ":" << endl;
-        for (const auto& connection : cities[i].connections) {
-            int cityIndex = connection.first;
-            int duration = connection.second;
-            cout << cities[cityIndex].name << " (Duration: " << duration << " minutes)" << endl;
-        }
-        cout << endl;
-    }
-
-    // Generate and print the duration matrix
-    cout << "Duration Matrix:" << endl;
-    vector<vector<int>> durationMatrix(numCities, vector<int>(numCities, INT_MAX));
-    for (int i = 0; i < numCities; i++) {
-        for (const auto& connection : cities[i].connections) {
-            int neighbor = connection.first;
-            int duration = connection.second;
-            durationMatrix[i][neighbor] = duration;
-            durationMatrix[neighbor][i] = duration;
-        }
-    }
-
-    for (int i = 0; i < numCities; i++) {
-        for (int j = 0; j < numCities; j++) {
-            if (durationMatrix[i][j] == INT_MAX) {
-                cout << "INF\t";
-            } else {
-                cout << durationMatrix[i][j] << "\t";
+            else if(ansGraph[i][j].first != 0){
+                cout<<input[i].name<<" pays Rs "<<ansGraph[i][j].first<<" to "<<input[j].name<<" via "<<ansGraph[i][j].second<<endl;
+                
             }
-        }
-        cout << endl;
-    }
-
-    int start, end;
-    cout << "Enter the starting city (1-" << numCities << "): ";
-    cin >> start;
-    cout << "Enter the ending city (1-" << numCities << "): ";
-    cin >> end;
-
-    // Get temperature input from the user
-    int temperature;
-    cout << "Enter the current temperature in Celsius: ";
-    cin >> temperature;
-
-    // Get fuel input from the user
-    int fuel;
-    cout << "Enter the remaining fuel in liters: ";
-    cin >> fuel;
-
-    // Find the shortest path using Dijkstra
-    vector<int> predecessors(numCities, -1);
-    vector<int> shortestPathDijkstra = dijkstra(cities, start - 1, end - 1, predecessors);
-
-    // Print the shortest path using Dijkstra
-    cout << "Shortest path from " << cities[start - 1].name << " to " << cities[end - 1].name << " (Dijkstra): ";
-    if (shortestPathDijkstra[end - 1] == INT_MAX) {
-        cout << "No path found";
-    } else {
-        cout << shortestPathDijkstra[end - 1] << " minutes" << endl;
-
-        // Print the cities in the shortest path
-        cout << "Cities in the shortest path: ";
-        int current = end - 1;
-        while (current != -1) {
-            cout << cities[current].name;
-            current = predecessors[current];
-            if (current != -1) {
-                cout << " <- ";
+            else if(ansGraph[j][i].first != 0){
+                cout<<input[j].name<<" pays Rs "<<ansGraph[j][i].first<<" to "<<input[i].name<<" via "<<ansGraph[j][i].second<<endl;
+                
             }
+            
+            ansGraph[i][j].first = 0;
+            ansGraph[j][i].first = 0;
         }
-        cout << endl;
     }
-
-    // Find the shortest paths using Floyd-Warshall
-    vector<vector<int>> shortestPathsFloydWarshall = floydWarshall(cities);
-
-    // Print the shortest path using Floyd-Warshall
-    cout << "\nShortest path from " << cities[start - 1].name << " to " << cities[end - 1].name << " (Floyd-Warshall): ";
-    if (shortestPathsFloydWarshall[start - 1][end - 1] == INT_MAX) {
-        cout << "No path found";
-    } else {
-        cout << shortestPathsFloydWarshall[start - 1][end - 1] << " minutes" << endl;
-    }
-
-    string weatherStatus = checkWeather(temperature);
-    cout << "Weather Status: " << weatherStatus << endl;
-
-    string fuelStatus = checkFuel(fuel);
-    cout << "Fuel Status: " << fuelStatus << endl;
-
-    return 0;
+    cout<<"\n";
 }
+
+void minimizeCashFlow(int numBanks,bank input[],unordered_map<string,int>& indexOf,int numTransactions,vector<vector<int>>& graph,int maxNumTypes){
+    
+    //Find net amount of each bank has
+    bank listOfNetAmounts[numBanks];
+    
+    for(int b=0;b<numBanks;b++){
+        listOfNetAmounts[b].name = input[b].name;
+        listOfNetAmounts[b].types = input[b].types;
+        
+        int amount = 0;
+        //incoming edges
+        //column travers
+        for(int i=0;i<numBanks;i++){
+            amount += (graph[i][b]);
+        }
+        
+        //outgoing edges
+        //row traverse
+        for(int j=0;j<numBanks;j++){
+            amount += ((-1) * graph[b][j]);
+        }
+        
+        listOfNetAmounts[b].netAmount = amount;
+    }
+    
+    vector<vector<pair<int,string>>> ansGraph(numBanks,vector<pair<int,string>>(numBanks,{0,""}));//adjacency matrix
+    
+    
+    //find min and max net amount
+    int numZeroNetAmounts=0;
+    
+    for(int i=0;i<numBanks;i++){
+        if(listOfNetAmounts[i].netAmount == 0) numZeroNetAmounts++;
+    }
+    while(numZeroNetAmounts!=numBanks){
+        
+        int minIndex=getMinIndex(listOfNetAmounts, numBanks);
+        pair<int,string> maxAns = getMaxIndex(listOfNetAmounts, numBanks, minIndex,input,maxNumTypes);
+        
+        int maxIndex = maxAns.first;
+        
+        if(maxIndex == -1){
+            
+            (ansGraph[minIndex][0].first) += abs(listOfNetAmounts[minIndex].netAmount);
+            (ansGraph[minIndex][0].second) = *(input[minIndex].types.begin());
+            
+            int simpleMaxIndex = getSimpleMaxIndex(listOfNetAmounts, numBanks);
+            (ansGraph[0][simpleMaxIndex].first) += abs(listOfNetAmounts[minIndex].netAmount);
+            (ansGraph[0][simpleMaxIndex].second) = *(input[simpleMaxIndex].types.begin());
+            
+            listOfNetAmounts[simpleMaxIndex].netAmount += listOfNetAmounts[minIndex].netAmount;
+            listOfNetAmounts[minIndex].netAmount = 0;
+            
+            if(listOfNetAmounts[minIndex].netAmount == 0) numZeroNetAmounts++;
+            
+            if(listOfNetAmounts[simpleMaxIndex].netAmount == 0) numZeroNetAmounts++;
+            
+        }
+        else{
+            int transactionAmount = min(abs(listOfNetAmounts[minIndex].netAmount), listOfNetAmounts[maxIndex].netAmount);
+            
+            (ansGraph[minIndex][maxIndex].first) += (transactionAmount);
+            (ansGraph[minIndex][maxIndex].second) = maxAns.second;
+            
+            listOfNetAmounts[minIndex].netAmount += transactionAmount;
+            listOfNetAmounts[maxIndex].netAmount -= transactionAmount;
+            
+            if(listOfNetAmounts[minIndex].netAmount == 0) numZeroNetAmounts++;
+            
+            if(listOfNetAmounts[maxIndex].netAmount == 0) numZeroNetAmounts++;
+        }
+        
+    }
+    
+    printAns(ansGraph,numBanks,input);
+    // cout<<"HI\n";
+}
+
+//correct
+int main() 
+{ 
+    cout<<"\n\t\t\t\t******** Welcome to CASH FLOW MINIMIZER SYSTEM ********\n\n\n";
+    cout<<"This system minimizes the number of transactions among multiple banks in the different corners of the world that use different modes of payment.There is one world bank (with all payment modes) to act as an intermediary between banks that have no common mode of payment. \n\n";
+    cout<<"Enter the number of banks participating in the transactions.\n";
+    int numBanks;cin>>numBanks;
+    
+    bank input[numBanks];
+    unordered_map<string,int> indexOf;//stores index of a bank
+    
+    cout<<"Enter the details of the banks and transactions as stated:\n";
+    cout<<"Bank name ,number of payment modes it has and the payment modes.\n";
+    cout<<"Bank name and payment modes should not contain spaces\n";
+    
+    int maxNumTypes;
+    for(int i=0; i<numBanks;i++){
+        if(i==0){
+            cout<<"World Bank : ";
+        }
+        else{
+            cout<<"Bank "<<i<<" : ";
+        }
+        cin>>input[i].name;
+        indexOf[input[i].name] = i;
+        int numTypes;
+        cin>>numTypes;
+        
+        if(i==0) maxNumTypes = numTypes;
+        
+        string type;
+        while(numTypes--){
+            cin>>type;
+            
+            input[i].types.insert(type);
+        }   
+        
+    }
+    
+    cout<<"Enter number of transactions.\n";
+    int numTransactions;
+    cin>>numTransactions;
+    
+    vector<vector<int>> graph(numBanks,vector<int>(numBanks,0));//adjacency matrix
+    
+    cout<<"Enter the details of each transaction as stated:";
+    cout<<"Debtor Bank , creditor Bank and amount\n";
+    cout<<"The transactions can be in any order\n";
+    for(int i=0;i<numTransactions;i++){
+        cout<<(i)<<" th transaction : ";
+        string s1,s2;
+        int amount;
+        cin >> s1>>s2>>amount;
+        
+        graph[indexOf[s1]][indexOf[s2]] = amount;
+    }
+     
+    //settle
+    minimizeCashFlow(numBanks,input,indexOf,numTransactions,graph,maxNumTypes);
+    return 0; 
+} 
+
+
+/*
+5
+A 2 t1 t2
+B 1 t1
+C 1 t1
+D 1 t2
+E 1 t2
+4
+B A 300
+C A 700
+D B 500
+E B 500
+
+--------
+5
+World_Bank 2 Google_Pay PayTM
+Bank_B 1 Google_Pay
+Bank_C 1 Google_Pay
+Bank_D 1 PayTM
+Bank_E 1 PayTM
+4
+Bank_B World_Bank 300
+World_Bank Bank_B 700
+Bank_D Bank_B 500
+Bank_E Bank_B 500
+
+--------------------
+
+6
+B 3 1 2 3
+C 2 1 2
+D 1 2
+E 2 1 3
+F 1 3
+G 2 2 3
+9
+G B 30
+G D 10
+F B 10
+F C 30
+F D 10
+F E 10
+B C 40
+C D 20
+D E 50
+*/
